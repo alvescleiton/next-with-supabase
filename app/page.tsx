10df8fac14ae220a0444e62831ebc5ec6a3b95c4
Cleiton Alves <cleiton_jc@yahoo.com.br>
2024-05-11 23:25:48 -0300

    session management

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,48 @@
 
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+
+  const getSession = async () => {
+    let {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession();
+
+    if (error) {
+      session = await refreshSession();
+    }
+
+    if (session) {
+      const { access_token, refresh_token } = session;
+
+      setSession(access_token, refresh_token);
+
+      return session;
+    } else {
+      handleLogout();
+    }
+  };
+
+  const refreshSession = async () => {
+    const {
+      data: { session },
+    } = await supabase.auth.refreshSession();
+
+    return session;
+  };
+
+  const setSession = async (access_token: string, refresh_token: string) => {
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+
+    return true;
+  };
 
   const handleLogout = async () => {
     let { error } = await supabase.auth.signOut();
@@ -13,6 +52,10 @@ export default function Home() {
 
     router.push("/login");
   };
+
+  useEffect(() => {
+    getSession();
+  }, []);
 
   return (
     <div>
